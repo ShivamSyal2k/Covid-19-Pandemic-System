dd317e0b16eb4918063699861488c2d1afb9e7d7
Shivam Syal <ssyal030@uottawa.ca>
2020-07-23 16:58:46 -0400

    This update adds the feature of adding symptoms and contacts to a patient profile by a nurse. It also allows the the lab to view a test and report the outcome of a test, the lab does not get to see the personal info of the patient, only there health card number just to prevent and baise when completing a test.

--- Covid19PandemicSystem/app/src/main/java/com/example/covid_19pandemicsystem/MainLab.java
+++ Covid19PandemicSystem/app/src/main/java/com/example/covid_19pandemicsystem/MainLab.java
@@ -6,31 +6,91 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import java.util.ArrayList;
 
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class MainLab extends AppCompatActivity {
 
-    TextView name;
-
+    private TextView healthCard, testLocation, testType, adminDate, adminTime;
+    private Button send, clear;
+    private EditText labLocationField, testOutcomeField;
+    public Patient patient;
+    public Test test;
+    public SharedPreferences sp2;
+    public String labLocation, testOutcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lab);
 
-        name = (TextView) findViewById(R.id.patientSize);
+        healthCard = (TextView) findViewById(R.id.healthCardNum);
+        testLocation = (TextView) findViewById(R.id.testLocation);
+        testType = (TextView) findViewById(R.id.testType);
+        adminDate = (TextView) findViewById(R.id.adminDate);
+        adminTime = (TextView) findViewById(R.id.adminTime);
+
+        send = (Button) findViewById(R.id.sendToDoctor);
+        clear = (Button) findViewById(R.id.clearLabInfo);
+
+        labLocationField = (EditText) findViewById(R.id.labLocation);
+        testOutcomeField = (EditText) findViewById(R.id.testOutcome);
+
+        sp2 = getSharedPreferences("labInfo", Context.MODE_PRIVATE);
 
         SharedPreferences sp =getApplicationContext().getSharedPreferences("PatientInfo", Context.MODE_PRIVATE);
+
         String nameString = sp.getString("patientName","");
         String addressString = sp.getString("patientAddress","");
         String phoneString = sp.getString("patientPhone","");
         int healthCardNum = sp.getInt("patientHeathNumber",0);
 
-        Patient temp = new Patient(nameString,addressString,phoneString,healthCardNum);
-        name.setText(nameString);
+        String dateAdminString = sp.getString("testDate","");
+        String timeAdminString = sp.getString("testTime","");
+        String testLocationString = sp.getString("testLocation","");
+        String testTypeString = sp.getString("testType","");
+
+        patient = new Patient(nameString,addressString,phoneString,healthCardNum);
+        //test = new Test(dateAdminString,timeAdminString,);
+
+        healthCard.setText("#: "+healthCardNum);
+        testLocation.setText("Location: "+testLocationString);
+        testType.setText("Type: "+testTypeString);
+        adminDate.setText("Date: "+dateAdminString);
+        adminTime.setText("Time: "+timeAdminString);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                labLocation = labLocationField.getText().toString();
+                testOutcome = testOutcomeField.getText().toString();
+
+                SharedPreferences.Editor editor = sp2.edit();
+                editor.putString("labLocation",labLocation);
+                editor.putString("testOutcome",testOutcome);
+                editor.commit();
+
+                healthCard.setText("#: ");
+                testLocation.setText("Location: ");
+                testType.setText("Type: ");
+                adminDate.setText("Date: ");
+                adminTime.setText("Time: ");
+                labLocationField.setText("");
+                testOutcomeField.setText("");
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                labLocationField.setText("");
+                testOutcomeField.setText("");
+            }
+        });
 
     }
 }
--- Covid19PandemicSystem/app/src/main/java/com/example/covid_19pandemicsystem/MainNursing.java
+++ Covid19PandemicSystem/app/src/main/java/com/example/covid_19pandemicsystem/MainNursing.java
@@ -10,18 +10,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.Date;
+
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+
 
 public class MainNursing extends AppCompatActivity {
-    private Button clear, submit;
-    private EditText patientName, address, phone, healthCardNumber, typeTest, locationTest, locationLab;
-    public String patientNameString, addressString, phoneString, testLocationString, testTypeString, labString;
+    private Button clear, submit, addSymptom, addContact;
+    private EditText patientName, address, phone, healthCardNumber, typeTest, locationTest, symptomField,startField, endField, contactNameField, contactDateField;
+    private SimpleDateFormat dateTime;
+    public String patientNameString, addressString, phoneString, testLocationString, testTypeString, timeString, dateString;
     public int healthNum;
     public Patient person;
     public SharedPreferences sp;
     public TestLocation testLocation;
     public TestType testType;
     public LabProcessingData lab;
+    public Test patientTest;
+    public ArrayList<String> symptoms, startDate, endDate, contactName,contactDate;
+    public ArrayList<SymptomDetails> symptomDetails;
+    public ArrayList<ContactDetails> contactDetails;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,18 @@ public class MainNursing extends AppCompatActivity {
 
         clear = (Button) findViewById(R.id.button1);
         submit = (Button) findViewById(R.id.button2);
+        addSymptom = (Button) findViewById(R.id.button3);
+        addContact = (Button) findViewById(R.id.button4);
+
+        symptoms = new ArrayList<String>();
+        startDate = new ArrayList<String>();
+        endDate = new ArrayList<String>();
+        contactName = new ArrayList<String>();
+        contactDate = new ArrayList<String>();
+
+        symptomDetails = new ArrayList<SymptomDetails>();
+        contactDetails = new ArrayList<ContactDetails>();
+
 
         patientName = (EditText) findViewById(R.id.patientNameField);
         address = (EditText) findViewById(R.id.addressField);
@@ -38,8 +65,13 @@ public class MainNursing extends AppCompatActivity {
 
         typeTest = (EditText) findViewById(R.id.typeTest);
         locationTest = (EditText) findViewById(R.id.locationTest);
-        locationLab = (EditText) findViewById(R.id.locationLab);
 
+        symptomField = (EditText) findViewById(R.id.symptomType);
+        startField = (EditText) findViewById(R.id.startSymptom);
+        endField = (EditText) findViewById(R.id.endSymptom);
+
+        contactDateField = (EditText) findViewById(R.id.contactDate);
+        contactNameField = (EditText) findViewById(R.id.contactName);
 
         sp = getSharedPreferences("PatientInfo", Context.MODE_PRIVATE);
 
@@ -61,13 +93,33 @@ public class MainNursing extends AppCompatActivity {
 
                 testTypeString = typeTest.getText().toString();
                 testLocationString = locationTest.getText().toString();
-                labString = locationLab.getText().toString();
 
                 createPatient();
                 createTest();
+                sendSymptoms();
+                sendContacts();
 
                 clear();
-                Toast.makeText(MainNursing.this,"Information Saved.",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainNursing.this,"Information Sent.",Toast.LENGTH_LONG).show();
+            }
+        });
+        addSymptom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createSymptoms();
+                symptomField.setText("");
+                startField.setText("");
+                endField.setText("");
+                Toast.makeText(MainNursing.this,"Symptom Saved.",Toast.LENGTH_LONG).show();
+            }
+        });
+        addContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createContacts();
+                contactNameField.setText("");
+                contactDateField.setText("");
+                Toast.makeText(MainNursing.this,"Contact Saved.",Toast.LENGTH_LONG).show();
             }
         });
 
@@ -93,12 +145,69 @@ public class MainNursing extends AppCompatActivity {
     public void createTest() {
         testLocation = new TestLocation(testLocationString);
         testType = new TestType(testTypeString);
-        lab = new LabProcessingData(labString);
+        lab = new LabProcessingData("");
+
+        dateTime = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date currentDate = new Date();
+        String dateAndTime = dateTime.format(currentDate);
+        dateString = dateAndTime.substring(0, 10);
+        timeString = dateAndTime.substring(11);
+
+        patientTest = new Test(dateString,timeString,"Null","Null",testLocation,testType,person,lab);
+        person.addTest(patientTest);
 
         SharedPreferences.Editor editor = sp.edit();
-        //editor.putString("testLocation",);
-        //editor.putString("testType",);
-        //editor.putString("labLocation",);
+        editor.putString("testLocation",patientTest.getTestLocation().getLocationType());
+        editor.putString("testType",patientTest.getTestType().getTestType());
+        editor.putString("testDate",dateString);
+        editor.putString("testTime",timeString);
+        editor.commit();
+    }
+    public void createSymptoms(){
+        String symp = symptomField.getText().toString();
+        symptoms.add(symp);
+
+        String startSymp = startField.getText().toString();
+        startDate.add(startSymp);
+
+        String endSymp = endField.getText().toString();
+        endDate.add(endSymp);
+
+        //SymptomDetails sympFull = new SymptomDetails(symp,startSymp,endSymp,person);
+        //symptomDetails.add(sympFull);
+
+        //person.addSymptomDetail(sympFull);
+
+    }
+    public void sendSymptoms(){
+        SharedPreferences.Editor editor = sp.edit();
+        Set<String> setSymp = new HashSet<String>(symptoms);
+        Set<String> setEnd = new HashSet<String>(endDate);
+        Set<String> setStart = new HashSet<String>(startDate);
+        editor.putStringSet("symptomsList",setSymp);
+        editor.putStringSet("symptomsEndList",setEnd);
+        editor.putStringSet("symptomsStartList",setStart);
+        editor.commit();
+    }
+    public void createContacts(){
+        String contactNameString = contactNameField.getText().toString();
+        contactName.add(contactNameString);
+
+        String contactDateString = contactDateField.getText().toString();
+        contactDate.add(contactDateString);
+
+       //ContactDetails contactFull = new ContactDetails(contactNameString,contactDateString);
+        //contactDetails.add(contactFull);
+
+       // person.addContactDetail(contactFull);
+
+    }
+    public  void sendContacts(){
+        SharedPreferences.Editor editor = sp.edit();
+        Set<String> setContactName = new HashSet<String>(contactName);
+        Set<String> setContactDate = new HashSet<String>(contactDate);
+        editor.putStringSet("contactList",setContactName);
+        editor.putStringSet("contactDateList",setContactDate);
         editor.commit();
     }
 }
